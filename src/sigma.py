8ab3807a6b5af5da726ca9ab7f5b6ab8fc537721
Paulo <galuzio.paulo@protonmail.com>
2020-01-17 10:20:31 -0500

    Simple doc for SquaredResidue function

--- src/sigma.py
+++ src/sigma.py
@@ -8,5 +8,16 @@ Solves the self consistent equation 4.151 from the Glendenning book.
 The goal is to find the value of the scalar field sigma in the equation
 """
 
-
 import numpy as np
+
+
+def _SqyuaredResidue(x, *args):
+    """
+    Squared residue is an internal module function that returns the squared of
+    the residue function.
+
+    Fiven y = f(x), the residue function is such as g(x) = y - f(x) = 0,
+    so solving for y = f(x) is the same is minimizing g(x)**2
+    """
+
+    return Residue**2
