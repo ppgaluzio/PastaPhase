4f7d8d2cc42f70dca738f59b1efd8d24a2d8bba7
Paulo <galuzio.paulo@protonmail.com>
2020-01-17 10:16:01 -0500

    Documentation for the module

--- src/sigma.py
+++ src/sigma.py
@@ -1,3 +1,12 @@
 # -*- coding: utf-8 -*-
 
+"""
+sigma.py
+--------
+
+Solves the self consistent equation 4.151 from the Glendenning book.
+The goal is to find the value of the scalar field sigma in the equation
+"""
+
+
 import numpy as np
