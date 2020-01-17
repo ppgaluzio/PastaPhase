846e657f643d93827ac80d3abcde3d5c00386e06
Paulo <galuzio.paulo@protonmail.com>
2020-01-17 10:26:08 -0500

    Inlcuded parameters to function

--- src/sigma.py
+++ src/sigma.py
@@ -11,13 +11,20 @@ The goal is to find the value of the scalar field sigma in the equation
 import numpy as np
 
 
-def _SquaredResidue(x, *args):
+def _SquaredResidue(s, gs, ms, m):
     """
     Squared residue is an internal module function that returns the squared of
     the residue function.
 
     Fiven y = f(x), the residue function is such as g(x) = y - f(x) = 0,
     so solving for y = f(x) is the same is minimizing g(x)**2
+
+    Parameters:
+    s: sigma
+    gs: g_sigma
+    ms: m_sigma
+    m: m
     """
+
 
     return Residue**2
