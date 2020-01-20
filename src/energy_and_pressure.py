321c8c0806cab96388a9a3f906273b5eb7ee9654
Paulo <galuzio.paulo@protonmail.com>
2020-01-20 06:37:57 -0500

    implement pressure function

--- src/energy_and_pressure.py
+++ src/energy_and_pressure.py
@@ -12,6 +12,32 @@ def _int_p(k, m, gs, sigma):
     return k**4 / np.sqrt(k**2 + (m - gs * sigma)**2)
 
 
+def pressure(ms, sigma, mw, w0, k, gs, m):
+    """
+    pressure
+    --------
+
+    calculate pressurey, eq. 4.160 from compact stars book
+
+    parameters:
+
+    ms: m_sigma
+    sigma: scalar field
+    mw: m_omega
+    w0: omega_0
+    k: fermi energy
+    gs: g_sigma
+    m: mass
+    """
+
+    p = - 0.5 * ms**2 * sigma**2 \
+        + 0.5 * mw**2 * w0**2 \
+        + (1/3) * (2/np.pi**2) \
+        * quad(_int_p, 0, k, args=(m, gs, sigma))
+
+    return p
+
+
 def energy(ms, sigma, mw, w0, k, gs, m):
     """
     energy
