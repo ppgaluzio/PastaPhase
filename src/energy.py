6691cecec7eef088ef203c38c6dea237e0ae5773
Paulo <galuzio.paulo@protonmail.com>
2020-01-20 06:25:06 -0500

    create energy function

---
+++ src/energy.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+
+def energy(ms, sigma, mw, w0, k, gs, m):
+    """
+    energy
+    ------
+
+    calculate energy density, eq. 4.160 from compact stars book
+
+    parameters:
+
+    ms: m_sigma
+    sigma: scalar field
+    mw: m_omega
+    w0: omega_0
+    k: fermi energy
+    gs: g_sigma
+    m: mass
+    """
+
+    return e
