494fe01ce3fd79077c07ffa04f532acae18581d6
Paulo <galuzio.paulo@protonmail.com>
2020-01-24 22:13:02 -0500

    State Function script implemented

--- bin/StateFunction.py
+++ bin/StateFunction.py
@@ -1,2 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as pl
+import numpy as np
+
+import pathmagic                # noqa
+
+import sigma as ss
+import energy_and_pressure as ep
+
+kk = np.linspace(0, 100, 50)
+ms = 100
+mw = 25
+m = 2
+gs = 150
+w0 = 0.2
+
+e = np.zeros_like(kk)
+p = np.zeros_like(kk)
+
+for i, k in enumerate(kk):
+    print(i, 'k = ', k)
+    sigma = ss.SolveSigma(gs, ms, m, k)
+    e[i] = ep.energy(ms, sigma, mw, w0, k, gs, m)
+    p[i] = ep.pressure(ms, sigma, mw, w0, k, gs, m)
+
+pl.plot(e, p)
+pl.show()
