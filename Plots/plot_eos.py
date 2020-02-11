581783540b36d16f7cd2f0d30c50f35f4b8841c5
Paulo <galuzio.paulo@protonmail.com>
2020-02-11 15:02:30 -0500

    change in plots

--- Plots/plot_eos.py
+++ Plots/plot_eos.py
@@ -2,29 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-data1 = np.loadtxt('tovqmc.dat')
+rho1, ener1, press1 = np.loadtxt('tovqmc.dat', unpack=True)
 data2 = np.loadtxt('tovtw.dat')
-
-ener1 = []
-press1 = []
 
 ener2 = []
 press2 = []
 
-#ener1, press1 = np.loadtxt(filename, unpack =True)
-
-for k in range((len(data1))):
-    ener1.append(data1[k,1])  
-    press1.append(data1[k,2])  
-        
 for k in range((len(data2))):
-    ener2.append(data2[k,1])  
-    press2.append(data2[k,2])  
+    ener2.append(data2[k, 1])
+    press2.append(data2[k, 2])
 
-
-
-plt.plot(ener1,press1,label="QMC")
-plt.plot(ener2,press2,label="TW")
+plt.plot(ener1, press1, label="QMC")
+plt.plot(ener2, press2, label="TW")
 #plt.axis([-10, 60, 0, 11])
 plt.title("EoS")
 plt.grid(True)
