692fee65aac66d813c55bf3bb437c72872af91a5
Guilherme Grams <grams.guilherme@gmail.com>
2020-02-14 19:02:07 +0100

    rho

--- bin/StateFunction.py
+++ bin/StateFunction.py
@@ -11,7 +11,7 @@ import energy_and_pressure as ep
 
 hc = 197.33                    # MeV.fm
 nB_inf = 0.05                 # minimum value for the barionic density in fm^-3 
-nB_sup = 1.00                 # maximum value for the barionic density in fm^-3 
+nB_sup = 0.70                 # maximum value for the barionic density in fm^-3 
 Npts = 200                     # number of points to the uniform matter EoS table
 
 #NLE set of parameters
@@ -24,7 +24,7 @@ mw = 782.501                    # omega mass in MeV
 mRho = 763.0                    # rho mass in MeV
 
 #defines proton fraction: Yp = (number of protons)/(number of barions)
-Yp = 0.5
+Yp = 0.3
 #nota pro Paulo, mudei a nomenclatura, rho > nB, eh o mais costumeiro. e assim posso chamar o meson rho, de rho.
 nB = np.linspace(nB_inf*hc**3,
                   nB_sup*hc**3,
@@ -42,7 +42,7 @@ for i, n in enumerate(nB):
     w0 = gw * n / mw**2                  # meson omega
     nB_p = Yp * n                        # density of protons
     nB_n = (1.0 - Yp ) * n               # density of electrons
-    rho = 0.5 * gRho * (nB_p - nB_n)) / mw**2          # meson rho
+    rho = 0.5 * gRho * (nB_p - nB_n) / mw**2          # meson rho
 
     sigma = ss.SolveSigma(gs, ms, m, k, tol=1.0e-5, n_seeds=100)
     print(i, 'k = ', k, ' sigma = ', sigma)
